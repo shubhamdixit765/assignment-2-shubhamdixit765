1f23b22bb68c53d2374203046685c750610cde4c
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2020-03-31 19:41:26 +0530

    assign the value of variable.

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -12,4 +12,11 @@ public class MyPriorityQueue {
     private Node rear;
     private Node front;
     private int size;
+
+    public MyPriorityQueue() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+
 }
