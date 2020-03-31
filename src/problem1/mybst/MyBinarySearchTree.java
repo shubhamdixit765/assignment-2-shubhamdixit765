7448764968648e7ac9428ecdfd92e2e4c3c4293f
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2020-03-31 19:22:10 +0530

    assign value of variable.

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -15,5 +15,39 @@ public class MyBinarySearchTree {
     public TreeNode root;
     private int count = 0;
 
+    public MyBinarySearchTree() {
+        this.root = null;
+        preOrderString = new StringBuilder();
+    }
+
+    public static void preOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        preOrder(root.getLeft());
+        preOrder(root.getRight());
+        preOrderString.append(root.getKey()).append(" ");
+    }
+
+    public void add(final int value) {
+        this.root = addService(this.root, value);
+    }
+
+    public TreeNode addService(final TreeNode treeRoot, final int addingValue) {
+        if (treeRoot == null) {
+            return new TreeNode(addingValue);
+        }
+        if (addingValue < treeRoot.getKey()) {
+            treeRoot.setLeft(addService(treeRoot.getLeft(), addingValue));
+        } else if (addingValue == treeRoot.getKey()) {
+            return treeRoot;
+        } else {
+            treeRoot.setRight(addService(treeRoot.getRight(), addingValue));
+        }
+
+        return treeRoot;
+    }
+
 
 }
