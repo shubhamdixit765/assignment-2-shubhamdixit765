1166575498e7603b397bccaefddd51d247743cff
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2020-03-31 19:53:18 +0530

    getter setter created.

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -45,4 +45,25 @@ public class MyQueue {
             temp = temp.getNext();
         }
     }
+
+    static class Node {
+        int data;
+        Node next;
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
