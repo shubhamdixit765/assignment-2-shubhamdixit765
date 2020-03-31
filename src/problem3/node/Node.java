17de953adab8c2cd7a648c8fa95dae7a62b3bb79
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2020-03-31 19:43:44 +0530

    getter setter created.

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -5,6 +5,35 @@
  *  Time: 7:13 PM
  */
 package problem3.node;
+import problem5.student.Student;
 
 public class Node {
+
+    private Student student;
+    private Node next;
+
+    /*
+     Constructor
+     */
+    public Node(Student student) {
+        this.student = student;
+        next = null;
+    }
+
+    /*
+    Getter & Setter methods
+     */
+    public Student getData() {
+        return student;
+    }
+
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 }
