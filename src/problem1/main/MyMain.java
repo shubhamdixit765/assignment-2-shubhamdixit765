291fca21fcaa4f8fdf1410b806f3243c8a40ae53
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2020-03-31 19:18:19 +0530

    import classes and object of them.

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,8 +7,30 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
     public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        Scanner scanner = new Scanner(System.in);
+
+        /*
+         *  taking only 7 inputs for simple demonstration with value 1,2,3,4,5,6,7
+         */
+        int i = 7;
+        System.out.println("Please take only 7 inputs");
+        while (i > 0) {
+            try {
+                tree.add(scanner.nextInt());
+            } catch (Exception e) {
+                System.out.println("Error Occurred is \" " + e + " \", so you are going to be kicked-out \n");
+                break;
+            }
+            i--;
+        }
+
 
     }
 }
