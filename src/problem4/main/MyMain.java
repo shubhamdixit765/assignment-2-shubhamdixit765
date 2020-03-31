d38b29247a308305838485b8e50cb07043173120
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2020-03-31 19:46:21 +0530

    creating void method.

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,5 +7,10 @@
 package problem4.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+
 public class MyMain {
+    public static void main(String[] args) {
+
+    }
 }
