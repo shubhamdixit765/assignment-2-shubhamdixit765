ae38a5e58446baab8e89b7aff5274313ac3883ea
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2020-03-31 19:42:37 +0530

    method created

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -54,4 +54,18 @@ public class MyPriorityQueue {
         }
     }
 
+    /*
+   helper method : to print the Queue
+    */
+    public void printQueue() {
+        Node temp = this.front;
+        for (int i = 0; i < this.size; i++) {
+            if (i != this.size - 1) {
+                System.out.print(temp.getData().toString() + "--->");
+                temp = temp.getNext();
+            } else {
+                System.out.println(temp.getData().toString());
+            }
+        }
+    }
 }
