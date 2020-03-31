9e20af1e8ca420f23996f0b7c6da02aa5247e5c6
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2020-03-31 19:52:00 +0530

    creating method.

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -6,6 +6,7 @@
  */
 package problem4.myqueue;
 // to create queue to store pre - order successor
+import problem1.node.TreeNode;
 
 public class MyQueue {
     private Node front;
@@ -16,4 +17,32 @@ public class MyQueue {
         rear = null;
     }
 
+    public void enqueue(int data) {
+        Node ns = new Node();
+        ns.setData(data);
+        if (front == null) {
+            front = rear = ns;
+        } else {
+            rear.setNext(ns);
+            rear = ns;
+        }
+    }
+
+    public void preSuccessor(TreeNode root) {
+        if (root == null)
+            return;
+        else {
+            enqueue(root.getKey());
+            preSuccessor(root.getLeft());
+            preSuccessor(root.getRight());
+        }
+    }
+
+    public void getPreSucc() {
+        Node temp = front.getNext();
+        while (temp != null) {
+            System.out.print(temp.getData() + "\t");
+            temp = temp.getNext();
+        }
+    }
 }
