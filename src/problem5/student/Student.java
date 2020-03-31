5962e5b01b77172c3f39c47522edc6c9482b276f
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2020-03-31 20:33:57 +0530

    declare variable.

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -8,4 +8,9 @@ package problem5.student;
 
 // to store student information and properties
 public class Student {
+    private int rollNumber;
+    private String Name;
+    private int backLogCounter;
+    private int appearingCounter;
+
 }
