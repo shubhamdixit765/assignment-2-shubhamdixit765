41b4490e45473624347fcf4cd20b705371d6d89d
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2020-03-31 19:25:18 +0530

    creating getter setter

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -11,4 +11,25 @@ public class TreeNode {
     private int key;
     private TreeNode left, right;
 
+    public TreeNode(int item) {
+        key = item;
+        left = right = null;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
 }
