1549d4a4b9bfaf789a98973eb05c7b94cb1e1d0c
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2020-03-31 20:04:22 +0530

    getter setter created

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -12,4 +12,26 @@ import problem5.student.Student;
 public class Node {
     private Student student;
     private Node next;
-}
+
+    public class Node {
+        private Student student;
+        private Node next;
+
+        public Node(Student student) {
+            this.student = student;
+            next = null;
+        }
+
+        public Student getData() {
+            return student;
+        }
+
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
