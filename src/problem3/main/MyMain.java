65b2449a4b79f0bf648cbdbde39f9a583a249508
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2020-03-31 19:35:29 +0530

    method created

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -7,8 +7,22 @@
 package problem3.main;
 // executable class
 // use problem5.student.Student class to create object of student
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+import problem5.student.Student;
+
 public class MyMain {
     public static void main(String[] args) {
-
+        MyPriorityQueue myPriorityQueue = new MyPriorityQueue();
+        /*
+         *   Adding 5 Students by hard-coding
+         *   The Queue enqueues them according to ascending Roll Number as given for example in Problem Statement
+         */
+        myPriorityQueue.enqueue(new Node(new Student(15, "GLA null")));
+        myPriorityQueue.enqueue(new Node(new Student(500, "NK  ")));
+        myPriorityQueue.enqueue(new Node(new Student(50, "VD ")));
+        myPriorityQueue.enqueue(new Node(new Student(11, "Jio kola")));
+        myPriorityQueue.enqueue(new Node(new Student(55, " DBC ")));
     }
 }
