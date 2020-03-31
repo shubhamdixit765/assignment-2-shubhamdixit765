a3d9548bb311463e92af6677b3c175f8eb185917
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2020-03-31 19:47:40 +0530

    import class and creating object

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -8,9 +8,28 @@ package problem4.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
 
 public class MyMain {
     public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.add(51);
+        tree.add(39);
+        tree.add(31);
+        tree.add(54);
+        tree.add(92);
+        tree.add(42);
+
+        MyQueue mq = new MyQueue();
+        /*
+         *   enQueueing from the tree pre-order successors of each node
+         */
+        mq.preSuccessor(tree.root);
+        System.out.print("\nPre-Order successor of all the nodes :-  ");
+        /*
+         *       Printing the Queue
+         */
 
     }
 }
